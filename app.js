09ef3ccf99c9514174f3bdef43c5d0d056994cb7
ildar <hilnur5@gmail.com>
2019-10-09 07:16:42 +0300

    fetch 150 pokemons asyncronously using promises

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const fetchPokemon = () => {
+    const promises = [];
   for (let i = 1; i <= 150; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    fetch(url)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        // console.log(data);
-        const pokemon = {
-          name: data.name,
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+
+  Promise.all(promises).then(results =>{
+      const pokemon = results.map((data)=>({
+        name: data.name,
           id: data.id,
           image: data.sprites["front_default"],
           type: data.types.map(type => type.type.name).join(", ")
+      }));
+      console.log(pokemon);
+  })
+
+
+      .then(data => {
+        // console.log(data);
+        const pokemon = {
+          
         };
 
         console.log(pokemon);
       });
-  }
+  
 };
 
 fetchPokemon();
