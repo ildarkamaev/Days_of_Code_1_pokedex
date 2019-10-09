37fd942d93c86ecce7085abce4385cbeb73fcd95
ildar <hilnur5@gmail.com>
2019-10-09 07:05:19 +0300

    fetch 150 pokemons using for loop

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const fetchPokemon = () =>{
-    const url = `https://pokeapi.co/api/v2/pokemon/1`;
+const fetchPokemon = () => {
+  for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     fetch(url)
-    .then(res=>{
+      .then(res => {
         return res.json();
-    })
-    .then(data=>{
+      })
+      .then(data => {
         // console.log(data);
         const pokemon = {
-            name: data.name,
-            id: data.id,
-            image: data.sprites['front_default'],
-            type: data.types.map(type => type.type.name).join(', ')
+          name: data.name,
+          id: data.id,
+          image: data.sprites["front_default"],
+          type: data.types.map(type => type.type.name).join(", ")
         };
-      
 
         console.log(pokemon);
-    })
-}
+      });
+  }
+};
 
 fetchPokemon();
