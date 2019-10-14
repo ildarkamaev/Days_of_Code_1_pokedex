372484791af625964c4f1ff3012f918eaa6df3d8
ildar <hilnur5@gmail.com>
2019-10-14 21:52:20 +0300

    back to normal v3

--- app.js
+++ app.js
@@ -1,55 +1,62 @@
-const pokedex = document.getElementById('pokedex');
+const pokedex = document.getElementById("pokedex");
 
-const fetchPokemon = () => {
-    const promises = [];
-  for (let i = 1; i <= 150; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
-  }
-
-  Promise.all(promises).then(results =>{
-      const pokemon = results.map((data)=>({
-        name: data.name,
-          id: data.id,
-          image: data.sprites["front_default"],
-          type: data.types.map(type => type.type.name).join(", ")
-      }));
-      displayPokemon(pokemon);
-      console.log(pokemon.type);
-  })
-  
+const fetchPokemon = async () => {
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+  const res = await fetch(url);
+  const data = await res.json();
+  const pokemon = data.results.map((result, index) => ({
+    name: result.name,
+    id: index + 1,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index +
+      1}.png`
+  }));
+  displayPokemon(pokemon);
 };
 
-<<<<<<< HEAD
 const displayPokemon = pokemon => {
   console.log(pokemon);
   const pokemonHTMLstring = pokemon
     .map(
       pokeman =>
         `<li class="card" onclick="selectPokemon(${pokeman.id})">
-            <div class="img-bg>
-              <img class="card-image" src="${pokeman.image}">
-            </div>
-=======
-const displayPokemon = (pokemon =>{
-    console.log(pokemon);
-    const pokemonHTMLstring = pokemon
-      .map(
-        pokeman =>
-          `<li class="card">
-            <img class="card-image" src="${pokeman.image}">
->>>>>>> parent of 83fd761... created popup in js
+        <img class="card-image" src="${pokeman.image}">
             <p class="card-subtitle">#${pokeman.id}</p>
             <h2 class="card-title">${pokeman.name}</h2>
-            <p class="card-subtitle">${pokeman.type}</p>
-        </li>`
-      )
-      .join("");
-    pokedex.innerHTML = pokemonHTMLstring;
-<<<<<<< HEAD
-});
-=======
-})
->>>>>>> parent of 83fd761... created popup in js
+             </li>`
+    )
+    .join("");
+  pokedex.innerHTML = pokemonHTMLstring;
+};
+
+const selectPokemon = async id => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokeman = await res.json();
+  displayPopUp(pokeman);
+};
+
+const displayPopUp = pokeman => {
+  const type = pokeman.types.map(type => type.type.name).join(", ");
+  const image = pokeman.sprites["front_default"];
+  const HTMLstring = `
+      <div class="popup">
+          <button id="closeBtn" onclick="closePopUp()">Close</button>
+          <li class="card"">
+          <img class="card-image" src="${image}">
+          <p class="card-subtitle">#${pokeman.id}</p>
+          <h2 class="card-title">${pokeman.name}</h2>
+      </li>
+      <p>Height: ${pokeman.height} | Weight: ${pokeman.weight} | Type: ${pokeman.type}</p>
+      </div>
+      
+    `;
+  pokedex.innerHTML = HTMLstring + pokedex.innerHTML;
+  console.log(HTMLstring);
+};
+
+const closePopUp = () => {
+  const popup = document.querySelector(".popup");
+  popup.parentElement.removeChild(popup);
+};
 
 fetchPokemon();
